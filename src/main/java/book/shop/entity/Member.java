81b9b77a6865c31a2e0eaf369bad72be3cc639d0
iizzzz <replicaset02@github.com>
2023-03-23 03:09:51 +0900

    Member Entity

--- src/main/java/book/shop/entity/Member.java
+++ src/main/java/book/shop/entity/Member.java
@@ -1,15 +1,20 @@
 package book.shop.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import book.shop.entity.embedded.Address;
+import jakarta.persistence.*;
 import lombok.Getter;
-import lombok.Setter;
 
-@Entity @Getter @Setter
+@Entity @Getter
 public class Member {
+
+    // PK
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
     private Long id;
+
+    @Column
     private String name;
+
+    @Embedded
+    private Address address;
 }
