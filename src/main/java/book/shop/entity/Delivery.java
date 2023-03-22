cd52b667dd94f922cff06e45348db14579dc57b5
iizzzz <replicaset02@github.com>
2023-03-23 03:10:32 +0900

    Delivery Entity

---
+++ src/main/java/book/shop/entity/Delivery.java
@@ -0,0 +1,29 @@
+package book.shop.entity;
+
+import book.shop.entity.embedded.Address;
+import book.shop.entity.status.DeliveryStatus;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity @Getter
+public class Delivery {
+
+    // PK
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "delivery_id")
+    private Long id;
+
+    // 배송과 주문은 1:1 관계이다
+    @OneToOne(mappedBy = "delivery")
+    @Setter
+    private Order order;
+
+    // 임베디드 값 타입
+    @Embedded
+    private Address address;
+
+    // 배송 상태 [READY, COMP]
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus status;
+}
