d934972d78a784a4435e6bd0f1aee2136d6e7530
iizzzz <replicaset02@github.com>
2023-03-24 02:18:50 +0900

    Repository 수정

--- src/main/java/book/shop/repository/MemberRepository.java
+++ src/main/java/book/shop/repository/MemberRepository.java
@@ -3,20 +3,36 @@ package book.shop.repository;
 import book.shop.entity.Member;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
 
-    @PersistenceContext
-    private EntityManager em;
+    private final EntityManager em;
 
-    public Long save(Member member) {
+    // 저장
+    public void save(Member member) {
         em.persist(member);
-        return member.getId();
     }
 
-    public Member find(Long id) {
+    // 단건 조회
+    public Member findOne(Long id) {
         return em.find(Member.class, id);
+    }
+
+    // 전체 조회
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class).getResultList();
+    }
+
+    // 파라미터 바인딩을 통해 이름으로 조회
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
     }
 }
